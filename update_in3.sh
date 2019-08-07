eb1b8d19d13fac068502895168e5a4c4f2452cd0
Simon Jentzsch <simon@slock.it>
2019-08-07 11:01:22 +0200

    update in3-script

--- update_in3.sh
+++ update_in3.sh
@@ -2,10 +2,18 @@
 
 IN3_SRC=git@github.com:SlockItEarlyAccess/in3-core.git
 cd app
-git clone $IN3_SRC
+
+if [ -d in3-core ]; then
+  cd in3-core
+  git pull
+  cd ..
+else
+  git clone $IN3_SRC
+fi
 
 # patch jni
 echo $"add_library(libin3 SHARED in3.c in3_IN3.c)\nSET_TARGET_PROPERTIES(libin3 PROPERTIES OUTPUT_NAME in3)\ntarget_link_libraries(libin3  eth_full)" > in3-core/src/bindings/java/CMakeLists.txt 
 
+# copy client to java path
 cp -r in3-core/src/bindings/java/in3 src/main/java/
 
