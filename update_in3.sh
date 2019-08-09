8c62ef15e801ccb3100fbfc533aae0df3e47b7c0
Simon Jentzsch <simon@slock.it>
2019-08-09 23:15:50 +0200

    update incubed

--- update_in3.sh
+++ update_in3.sh
@@ -11,9 +11,8 @@ else
   git clone $IN3_SRC
 fi
 
-# patch jni
-echo $"add_library(libin3 SHARED in3.c in3_IN3.c)\nSET_TARGET_PROPERTIES(libin3 PROPERTIES OUTPUT_NAME in3)\ntarget_link_libraries(libin3  eth_full)" > in3-core/src/bindings/java/CMakeLists.txt 
 
 # copy client to java path
 cp -r in3-core/src/bindings/java/in3 src/main/java/
-
+# but not the native libs
+rm -rf src/main/java/in3/native
